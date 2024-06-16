7f04c47882777da23161caf99ee01e3164a24776
Pierre DEFAUQUET <pierredefauquet@Pierres-MBP.lan>
2024-06-16 20:40:12 +0200

    maj#12 configuration de DisqueCrudController

--- src/Controller/Admin/DisqueCrudController.php
+++ src/Controller/Admin/DisqueCrudController.php
@@ -3,10 +3,17 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Disque;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use App\Entity\Produit;
+
+
 
 class DisqueCrudController extends AbstractCrudController
 {
@@ -15,14 +22,22 @@ class DisqueCrudController extends AbstractCrudController
         return Disque::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->hideOnForm(),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('subtitle'),
+            TextareaField::new('description'),
+            NumberField::new('price'),
+    
+    
+            AssociationField::new('artiste'),
+            // AssociationField::new('images')
+            //     ->setBasePath('images/')
+            //     ->setUploadDir('public/images/imgDisc')
+            //     ->setRequired($pageName !== Crud::PAGE_EDIT)
+            //     ->setFormTypeOptions($pageName !== Crud::PAGE_EDIT ? ['allow_delete' => false] : []),
         ];
     }
-    */
 }
